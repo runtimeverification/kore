3dc242f149d044dd94dbd36f7e1ef29ae16967cb
RV Jenkins <admin@runtimeverification.com>
2021-10-19 12:18:06 +0000

    gh-pages: Updated the website

--- test/regression-evm/test-branching-invalid.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-${KORE_EXEC:?} test-branching-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-branching-invalid-tmp.in.qYUUQOY4i6 --searchType FINAL --search test-branching-invalid-tmp.pattern.mJgXmabpAR "$@"
--- test/regression-evm/test-branching-no-invalid.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-${KORE_EXEC:?} test-branching-no-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-branching-no-invalid-tmp.in.QtnoXkZKiq --searchType FINAL --search test-branching-no-invalid-tmp.pattern.d19duwx71Y "$@"
