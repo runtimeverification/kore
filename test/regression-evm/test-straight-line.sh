77ce9802e69d64ab15547aa38cc5f67a834a1ce3
RV Jenkins <admin@runtimeverification.com>
2021-11-17 14:44:17 +0000

    gh-pages: Updated the website

--- test/regression-evm/test-straight-line.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-${KORE_EXEC:?} test-straight-line-definition.kore --module ETHEREUM-SIMULATION --pattern test-straight-line-tmp.in.ah2XsTm0Ic --searchType FINAL --search test-straight-line-tmp.pattern.XWhzepgj60 "$@"
