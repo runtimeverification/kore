34c5c5c43ed447da526398fe53dc024d5a0c633c
RV Jenkins <admin@runtimeverification.com>
2021-12-14 16:13:00 +0000

    gh-pages: Updated the website

--- docker/build.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker build --build-arg K_COMMIT=$(cat deps/k_release | cut -c2- ) --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) -t kore .
--- test/regression-evm/test-totalSupply.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-${KORE_EXEC:?} test-totalSupply-vdefinition.kore --module VERIFICATION --prove test-totalSupply-spec.kore --spec-module TOTALSUPPLY-SPEC "$@"
