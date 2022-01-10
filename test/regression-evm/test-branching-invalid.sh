fed2b35af12474ad656af57ad6d2230cef3d4a31
RV Jenkins <admin@runtimeverification.com>
2022-01-10 17:44:39 +0000

    gh-pages: Updated the website

--- test/regression-evm/test-branching-invalid.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-${KORE_EXEC:?} test-branching-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-branching-invalid-tmp.in.vh7789L6AJ --searchType FINAL --search test-branching-invalid-tmp.pattern.mWnbxncnmO "$@"
--- test/regression-evm/test-straight-line-no-invalid.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-${KORE_EXEC:?} test-straight-line-no-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-straight-line-no-invalid-tmp.in.tlyP1AHHkv --searchType FINAL --search test-straight-line-no-invalid-tmp.pattern.gHG45x3gE0 "$@"
