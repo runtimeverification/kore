26bda3e66db969119a63a0593e0c94fa072874bf
RV Jenkins <admin@runtimeverification.com>
2022-01-25 12:49:48 +0000

    gh-pages: Updated the website

--- test/regression-evm/test-branching-invalid.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-${KORE_EXEC:?} test-branching-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-branching-invalid-tmp.in.ir6QLXj9N1 --searchType FINAL --search test-branching-invalid-tmp.pattern.OSkOXcn2Pe "$@"
